3e1f9cf6a1d348150a5b2d209bcef12fd9540f41
Simon Kreuz <simonkreuz@me.com>
2015-10-17 17:42:02 -0700

    Added Heroku settings

--- oneminutelabel/settings.py
+++ oneminutelabel/settings.py
@@ -127,4 +127,13 @@ LOGGING = {
 
 SHIPPO_TOKEN = os.getenv("SHIPPO_TOKEN")
 STRIPE_TOKEN = os.getenv("STRIPE_TOKEN")
-STRIPE_DESCRIPTION = "One Minute Label Postage"
+STRIPE_DESCRIPTION = "One Minute Label Postage"# Heroku
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
